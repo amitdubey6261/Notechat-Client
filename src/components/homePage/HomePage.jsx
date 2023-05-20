93828782ae94353f3da44bfe45c795a935717c63
amit dubey <amitdubey6261@gmail.com>
2023-05-20 19:29:56 +0530

    images

--- src/components/homePage/HomePage.jsx
+++ src/components/homePage/HomePage.jsx
@@ -4,6 +4,8 @@ import './homePage.css'
 
 import image from '../../assets/images/myimg.png'
 
+import Card from './cards/Card';
+
 const HomePage = () => {
     return (
         <>
@@ -18,6 +20,60 @@ const HomePage = () => {
                             <div className="img-o-c-t"><span id="">Share your content around the world</span></div>
                         </div>
                     </div>
+                </div>
+                <div className="category-navigation">
+                    <div className="navigation-wrapper">
+                        <div className="navigation-element"> <span className='navigation-text' id='heading'>NOTES</span> </div>
+                        <div className="navigation-element"> <span className='navigation-text'>I Semester</span> </div>
+                        <div className="navigation-element"> <span className='navigation-text'>II Semester</span> </div>
+                        <div className="navigation-element"> <span className='navigation-text'>III Semester</span> </div>
+                        <div className="navigation-element"> <span className='navigation-text'>IV Semester</span> </div>
+                        <div className="navigation-element"> <span className='navigation-text'>V Semester</span> </div>
+                        <div className="navigation-element"> <span className='navigation-text'>VI Semester</span> </div>
+                        <div className="navigation-element"> <span className='navigation-text'>VII Semester</span> </div>
+                        <div className="navigation-element"> <span className='navigation-text'>VIII Semester</span> </div>
+                    </div>
+                </div>
+                <div className="elements-space">
+                    <div ><span className="heading">COLLECTION</span></div>
+                    <div ><span className="sub-heading">Everything you need for your fitness journey is right here</span></div>
+                    <div className="card-container">
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                </div>
                 </div>
             </main>
         </>
--- src/components/homePage/cards/Card.jsx
+++ src/components/homePage/cards/Card.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import './card.css'
+
+import image from '../../../assets/images/img1.png';
+import image2 from '../../../assets/images/myimg3.png';
 
 const Card = () => {
+  let arr = [] ; 
+  arr.push( image ) ;
+  arr.push( image2 ) ;
+  console.warn( arr[0] , arr[1]);
   return (
-    <div>Card</div>
+    <>
+      <div className="card">
+        <img className='card-image' src={Math.random() < 0.5 ? arr[0] : arr[1]} alt="not found" />
+        <div className="card-overlay">
+          <p className='card-text'>This Is Devloped By Amit  </p>
+        </div>
+      </div>
+    </>
   )
 }
 
